e27a046b28cbae62e4f017b205f39edbb34d95cf
Itai Bar-Haim <itai.barhaim@applitools.com>
2017-10-17 09:05:41 +0300

    added logger to Region.

--- eyes.sdk.core/src/main/java/com/applitools/eyes/EyesBase.java
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/EyesBase.java
@@ -103,6 +103,9 @@ public abstract class EyesBase {
         ArgumentGuard.notNull(serverUrl, "serverUrl");
 
         logger = new Logger();
+
+        Region.initLogger(logger);
+
         scaleProviderHandler = new SimplePropertyHandler<>();
         scaleProviderHandler.set(new NullScaleProvider());
         cutProviderHandler = new SimplePropertyHandler<>();
